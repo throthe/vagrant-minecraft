72091fdd9648c521f293cce40bb5949d7623fd5c
throthe <nomad.blow@gmail.com>
2018-02-06 11:17:52 +0800

    comment parts from oracle jdk

--- provision/jdk.sh
+++ provision/jdk.sh
@@ -2,9 +2,10 @@
 # Uninstall openjdk
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y remove openjdk*   
 
-# Add Repo
-sudo add-apt-repository ppa:webupd8team/java
-# Update apt-get
-sudo apt-get update
+# Add Repo (unsed)
+# sudo add-apt-repository ppa:webupd8team/java
+
+# Update apt-get (unsed)
+# sudo apt-get update
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install openjdk-8-jre
